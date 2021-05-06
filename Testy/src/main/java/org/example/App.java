97c69ae7e641f30ea98a5efddb772bb1674f703b
Michal Musial <michal.musial@ovoo.pl>
2021-05-06 16:11:47 +0200

    Inna

--- Testy/src/main/java/org/example/App.java
+++ Testy/src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Pierwsza zmiana");
         System.out.println("Druga zmiana");
+        System.out.println("Inna zmiana");
     }
 }
